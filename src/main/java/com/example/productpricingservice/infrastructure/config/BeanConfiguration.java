25f22e68581238fcbdad54a91eddd370408d785f
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2026-04-19 09:44:10 +0200

    chore: Remove redundant files

--- src/main/java/com/example/productpricingservice/infrastructure/config/BeanConfiguration.java
+++ src/main/java/com/example/productpricingservice/infrastructure/config/BeanConfiguration.java
@@ -6,15 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import com.example.productpricingservice.application.port.in.GetApplicablePriceUseCase;
 import com.example.productpricingservice.application.port.out.ProductPriceRepository;
 import com.example.productpricingservice.application.usecase.GetApplicablePriceService;
-import com.example.productpricingservice.domain.service.PriceSelectionPolicy;
 
 @Configuration
 public class BeanConfiguration {
-
-    @Bean
-    public PriceSelectionPolicy priceSelectionPolicy() {
-        return new PriceSelectionPolicy();
-    }
 
     @Bean
     public GetApplicablePriceUseCase getApplicablePriceUseCase(
